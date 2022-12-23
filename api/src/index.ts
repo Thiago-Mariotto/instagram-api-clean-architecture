5ea6e98b988fd69fb9594663f39f333f6d632465
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-12-23 19:56:38 -0300

    refactor: add router express to guide routes

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,16 +1,9 @@
 import dotenv from 'dotenv';
-import CreateUser from './application/use_cases/CreateUser';
-import GetUsers from './application/use_cases/GetUsers';
-import UserController from './infra/controller/UserController';
 import ExpressAdapter from './infra/http/ExpressAdapter';
-import UserMemoryRepository from './infra/repository/memory/UserMemoryRepository';
 
 dotenv.config();
-const PORT = process.env.PORT || 3001;
-const userMemoryRepository = new UserMemoryRepository();
-const httpServer = new ExpressAdapter();
-const createUser = new CreateUser(userMemoryRepository);
-const getUsers = new GetUsers(userMemoryRepository);
 
-new UserController(httpServer, createUser, getUsers);
+const PORT = process.env.PORT || 3001;
+const httpServer = new ExpressAdapter();
+
 httpServer.listen(PORT as number);
