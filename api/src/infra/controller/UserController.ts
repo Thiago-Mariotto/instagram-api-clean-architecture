a2a15a005a28c1574c71c998fd1a4f9d1b9813d2
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-12-23 13:05:03 -0300

    feat: add get user method

--- api/src/infra/controller/UserController.ts
+++ api/src/infra/controller/UserController.ts
@@ -1,18 +1,25 @@
 import CreateUser from '../../application/use_cases/CreateUser';
+import GetUsers from '../../application/use_cases/GetUsers';
 import HttpServer from '../http/HttpServer';
 
 export default class UserController {
   constructor(
     readonly httpServer: HttpServer,
-    readonly createUser: CreateUser
+    readonly createUser: CreateUser,
+    readonly getUsers: GetUsers
   ) {
-    httpServer.on('post', '/user', async function (body: any) {
+    httpServer.on('post', '/user', async function (_params: any, body: any) {
       const input = {
         username: body.username,
         email: body.email,
         password: body.password
       };
       await createUser.execute(input);
+    });
+
+    httpServer.on('get', '/user', async function (_params: any, _body: any) {
+      const users = await getUsers.execute();
+      return users;
     });
   }
 }
