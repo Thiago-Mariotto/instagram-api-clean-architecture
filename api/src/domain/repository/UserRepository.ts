67205a530447bd452127fc6092015928934ae45f
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-12-23 13:04:25 -0300

    feat: implements list methods to user repository

--- api/src/domain/repository/UserRepository.ts
+++ api/src/domain/repository/UserRepository.ts
@@ -2,7 +2,8 @@ import User from '../entity/User';
 
 interface UserRepository {
   saveUser(user: User): Promise<void>;
-  getUserById(id: string): Promise<User>;
+  getUserById(id: string): Promise<User | undefined>;
+  getUsers(): Promise<User[]>;
 }
 
 export default UserRepository;
