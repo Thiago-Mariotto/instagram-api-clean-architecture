679d2b1d5ddf4277ea3b26c36a7a90752eea8e56
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-12-26 12:42:40 -0300

    refactor: add user controller

--- api/src/infra/routes/UserRouter.ts
+++ api/src/infra/routes/UserRouter.ts
@@ -1,43 +1,34 @@
 import { NextFunction, Request, Response, Router } from 'express';
-import Photography from '../../domain/entity/Photography';
-import S3Storage from '../../domain/entity/S3Storage';
-import User from '../../domain/entity/User';
-import PhotoRepository from '../../domain/repository/PhotoRepository';
+import PhotoRepository from '../../domain/repository/PhotographyRepository';
 import UserRepository from '../../domain/repository/UserRepository';
+import UserController from '../controller/UserController';
 import FileHandler from '../implementations/FileHandler';
 
 export default class UserRouter {
   route: Router;
-  fileHandler: any;
+  fileHandler: FileHandler;
+  userController: UserController;
 
-  constructor(readonly userRepository: UserRepository, readonly photoRepository: PhotoRepository) {
+  constructor(
+    readonly userRepository: UserRepository,
+    readonly photoRepository: PhotoRepository
+  ) {
     this.route = Router();
     this.fileHandler = new FileHandler();
+    this.userController = new UserController(userRepository, photoRepository);
 
     this.route.get('/user', async (req: Request, res: Response, _next: NextFunction) => {
-      const users = await this.userRepository.getUsers();
-      return res.status(200).json(users);
+      this.userController.getAll(req, res);
     });
 
     this.route.post('/user', async (req: Request, res: Response, _next: NextFunction) => {
-      const body = req.body;
-      const user = new User(body.username, body.email, body.password);
-      this.userRepository.saveUser(user);
-      return res.status(201).end();
+      this.userController.create(req, res);
     });
 
-    this.route.post('/user/:id/upload', this.fileHandler.upload().single('file'), async (req: Request, res: Response, _next: NextFunction) => {
-      const { tmpFileName, tmpFilePath } = req.body;
-      const ownerId = req.params.id;
-
-      const s3Storage = new S3Storage();
-      await s3Storage.saveFile(tmpFilePath, tmpFileName);
-
-      const fileUrl = `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${tmpFileName}`;
-      const photo = new Photography(ownerId, fileUrl);
-      photoRepository.save(photo);
-      this.fileHandler.removeUploadedFile(tmpFilePath);
-      return res.status(201).send('ok');
-    });
+    this.route.post('/user/:id/upload',
+      this.fileHandler.upload().single('file'),
+      async (req: Request, res: Response, _next: NextFunction) => {
+        this.userController.uploadFile(req, res);
+      });
   }
 }
