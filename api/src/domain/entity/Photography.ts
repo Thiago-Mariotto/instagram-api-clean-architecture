6d67ba01bd9fbff5189176343a8fe2f629198c67
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-12-26 12:39:54 -0300

    fix: use id attribute on photo

--- api/src/domain/entity/Photography.ts
+++ api/src/domain/entity/Photography.ts
@@ -1,14 +1,14 @@
 import Identifier from './Identifier';
 
 export default class Photography {
-  identifier: Identifier;
+  identifier: string;
   ownerId: string;
   active: boolean;
   url: string;
   createdAt: Date;
 
   constructor(ownerId: string, url: string) {
-    this.identifier = new Identifier();
+    this.identifier = new Identifier().id;
     this.ownerId = ownerId;
     this.active = true;
     this.url = url;
