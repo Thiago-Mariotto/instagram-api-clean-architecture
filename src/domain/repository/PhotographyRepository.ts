5087e9fd287ba656d5cb4926e2bfb7f1eb42d589
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-12-31 17:37:16 -0300

    chore: possible undefined return

--- src/domain/repository/PhotographyRepository.ts
+++ src/domain/repository/PhotographyRepository.ts
@@ -1,7 +1,7 @@
 import Photography from '../entity/Photography';
 interface PhotographyRepository {
   save(photo: Photography): Promise<void>;
-  getById(id: string): Promise<Photography[]>
+  getById(id: string): Promise<Photography | undefined>
 }
 
 export default PhotographyRepository;
