12d17cf45ae9615b01f39d65780c0f210be51bc9
Thiago Mariotto <thiagomariotto@outlook.com.br>
2022-12-31 17:31:38 -0300

    chore: implements user repository methods

--- src/domain/repository/UserRepository.ts
+++ src/domain/repository/UserRepository.ts
@@ -3,7 +3,8 @@ import User from '../entity/User';
 interface UserRepository {
   saveUser(user: User): Promise<void>;
   getUserById(id: string): Promise<User | undefined>;
-  getUsers(): Promise<User[]>;
+  updateUser(user: User): Promise<void>;
+  removeUser(id: string): Promise<void>;
 }
 
 export default UserRepository;
