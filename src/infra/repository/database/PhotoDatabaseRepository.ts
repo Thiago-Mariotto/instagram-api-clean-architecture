4f45505a5a03a01c88abb1b28125d5fea4828693
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-01-03 18:28:42 -0300

    fix: add try/catch

--- src/infra/repository/database/PhotoDatabaseRepository.ts
+++ src/infra/repository/database/PhotoDatabaseRepository.ts
@@ -3,14 +3,20 @@ import PhotoRepository from '../../../domain/repository/PhotographyRepository';
 import Connection from '../../database/Connection';
 
 export default class PhotoDatabaseRepository implements PhotoRepository {
-
   constructor(readonly connection: Connection) { }
   async getById(id: string): Promise<Photography | undefined> {
-    return await this.connection.query('SELECT * FROM photos WHERE id = ?', [id]);
+    try {
+      return await this.connection.query('SELECT * FROM photos WHERE id = ?', [id]);
+    } catch (error: any) {
+      console.log(`Error on SELECT Photos, ${error.message}`);
+    }
   }
-
   async save(photo: Photography): Promise<void> {
-    this.connection.query('INSERT INTO photos (id, owner_id, active, url, createad_at) VALUES (?, ?, ?, ?, ?)',
-      [photo.identifier, photo.ownerId, photo.active, photo.url, photo.createdAt]);
+    try {
+      this.connection.query('INSERT INTO photos (id, owner_id, active, url, createad_at) VALUES (?, ?, ?, ?, ?)',
+        [photo.identifier, photo.ownerId, photo.active, photo.url, photo.createdAt]);
+    } catch (error: any) {
+      console.log(`Error on SELECT Photos, ${error.message}`);
+    }
   }
 }
