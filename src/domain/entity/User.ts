3993a411416b9f0c4bbe923ccddb01222b05d410
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-01-03 18:53:05 -0300

    feat: hash password to save

--- src/domain/entity/User.ts
+++ src/domain/entity/User.ts
@@ -1,23 +1,35 @@
 import joi from 'joi';
+import HashPassword from './HashPassword';
 import Identifier from './Identifier';
 
 export default class User {
-  id: string;
+  readonly id: string;
+
   username: string;
   email: string;
   password: string;
+  private hashPassword: HashPassword;
+  private MIN_PASSWORD_LENGTH = 6;
+  private MAX_PASSWORD_LENGTH = 24;
 
   constructor(username: string, email: string, password: string) {
+    this.hashPassword = new HashPassword();
     this.id = new Identifier().id;
+    this.passIsValid(password);
     this.username = username;
     this.email = email;
-    this.passIsValid(password);
-    this.password = password;
+    this.password = this.encryptPass(password);
   }
 
   private passIsValid(password: string) {
-    const PWDSCHEMA = joi.string().min(6).max(16);
+    const PWDSCHEMA = joi.string()
+      .min(this.MIN_PASSWORD_LENGTH)
+      .max(this.MAX_PASSWORD_LENGTH);
     const { error } = PWDSCHEMA.validate(password);
-    if (error) throw Error('Invalid password');
+    if (error) { console.log(error); throw Error('Invalid password'); }
+  }
+
+  private encryptPass(password: string) {
+    return this.hashPassword.encrypt(password);
   }
 }
