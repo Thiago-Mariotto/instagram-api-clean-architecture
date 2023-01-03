70a978d6fe6157e391f141b6f3c9bdcb1e25b58e
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-01-03 18:52:49 -0300

    chore: implements handler middleware error

--- src/infra/http/ExpressAdapter.ts
+++ src/infra/http/ExpressAdapter.ts
@@ -1,14 +1,18 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import Router from '../routes/MainRouter';
 export default class ExpressAdapter {
   app: any;
 
   constructor() {
+    const router = new Router();
+
     this.app = express();
     this.app.use(express.json());
-    const router = new Router();
     this.app.use(router.route);
+    this.app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+      return res.status(error.status || 500).json({ message: error.message || 'Internal Server Error' });
+    });
   }
 
   listen(port: number): void {
