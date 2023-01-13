0aad5d772f2f0cc2c914aedb4dfe8db32c9a5c23
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-01-13 20:55:38 -0300

    feat: implements user validations

--- src/domain/entity/User.ts
+++ src/domain/entity/User.ts
@@ -5,9 +5,9 @@ import Identifier from './Identifier';
 export default class User {
   readonly id: string;
 
-  username: string;
-  email: string;
-  password: string;
+  private username: string;
+  private email: string;
+  private password: string;
   private hashPassword: HashPassword;
   private MIN_PASSWORD_LENGTH = 6;
   private MAX_PASSWORD_LENGTH = 24;
@@ -15,21 +15,42 @@ export default class User {
   constructor(username: string, email: string, password: string) {
     this.hashPassword = new HashPassword();
     this.id = new Identifier().id;
-    this.passIsValid(password);
     this.username = username;
     this.email = email;
+    this.validateFields(email, password);
     this.password = this.encryptPass(password);
+  }
+
+  private validateFields(email: string, password: string) {
+    this.passIsValid(password);
+    this.emailIsValid(email);
   }
 
   private passIsValid(password: string) {
     const PWDSCHEMA = joi.string()
       .min(this.MIN_PASSWORD_LENGTH)
-      .max(this.MAX_PASSWORD_LENGTH);
+      .max(this.MAX_PASSWORD_LENGTH)
+      .required();
     const { error } = PWDSCHEMA.validate(password);
     if (error) { throw Error('Invalid password'); }
   }
 
+  private emailIsValid(email: string) {
+    const EMAILSCHEMA = joi.string()
+      .email()
+      .required();
+    const { error } = EMAILSCHEMA.validate(email);
+    if (error) { throw Error('Invalid email') }
+  }
+
   private encryptPass(password: string) {
     return this.hashPassword.encrypt(password);
+  }
+
+  public getUser() {
+    return {
+      username: this.username,
+      email: this.email,
+    }
   }
 }
