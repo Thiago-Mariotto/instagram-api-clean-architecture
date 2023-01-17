0ce85c6712e28a5392d3e2eaf067e35da8412877
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-01-17 20:12:07 -0300

    chore: change user entity

--- src/domain/entity/User.ts
+++ src/domain/entity/User.ts
@@ -1,38 +1,45 @@
 import joi from 'joi';
-import HashPassword from './HashPassword';
 import Identifier from './Identifier';
 
 export default class User {
   readonly identifier: string;
+  private _username: string;
+  private _email: string;
+  private _password: string;
 
-  private username: string;
-  private email: string;
-  private password: string;
-  private hashPassword: HashPassword;
-  private MIN_PASSWORD_LENGTH = 6;
-  private MAX_PASSWORD_LENGTH = 24;
+  private _MIN_PASSWORD_LENGTH = 6;
+  private _MAX_PASSWORD_LENGTH = 24;
 
   constructor(username: string, email: string, password: string) {
-    this.hashPassword = new HashPassword();
+    this.validateFields(username, email, password);
     this.identifier = new Identifier().id;
-    this.username = username;
-    this.email = email;
-    this.validateFields(email, password);
-    this.password = this.encryptPass(password);
+    this._username = username;
+    this._email = email;
+    this._password = password;
   }
 
-  private validateFields(email: string, password: string) {
+  private validateFields(username: string, email: string, password: string) {
     this.passIsValid(password);
     this.emailIsValid(email);
+    this.userNameIsValid(username);
   }
 
   private passIsValid(password: string) {
     const PWDSCHEMA = joi.string()
-      .min(this.MIN_PASSWORD_LENGTH)
-      .max(this.MAX_PASSWORD_LENGTH)
+      .min(this._MIN_PASSWORD_LENGTH)
+      .max(this._MAX_PASSWORD_LENGTH)
       .required();
     const { error } = PWDSCHEMA.validate(password);
     if (error) { throw Error('Invalid password'); }
+  }
+
+  private userNameIsValid(username: string) {
+    const USERNAMESCHEMA = joi.string()
+      .min(4)
+      .required();
+
+    const { error } = USERNAMESCHEMA.validate(username);
+    if (error) { throw Error('Invalid username') }
   }
 
   private emailIsValid(email: string) {
@@ -43,15 +50,36 @@ export default class User {
     if (error) { throw Error('Invalid email') }
   }
 
-  private encryptPass(password: string) {
-    return this.hashPassword.encrypt(password);
-  }
-
   public getUser() {
     return {
       identifier: this.identifier,
-      username: this.username,
-      email: this.email,
+      username: this._username,
+      email: this._email,
     }
+  }
+
+  public get email(): string {
+    return this._email;
+  }
+  public set email(value: string) {
+    this.emailIsValid(value);
+    this._email = value;
+  }
+
+  public get username(): string {
+    return this._username;
+  }
+  public set username(value: string) {
+    this.userNameIsValid(value);
+    this._username = value;
+  }
+
+  public get password(): string {
+    return this._password;
+  }
+
+  public set password(value: string) {
+    this.passIsValid(value);
+    this._password = value;
   }
 }
