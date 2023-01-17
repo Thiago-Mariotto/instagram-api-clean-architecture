ad66d6d1c77451108e7207057af4da62a8d8a15f
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-01-17 15:53:19 -0300

    refactor: change attributes scope

--- src/domain/entity/Photo.ts
+++ src/domain/entity/Photo.ts
@@ -1,17 +1,35 @@
 import Identifier from './Identifier';
 
 export default class Photo {
-  identifier: string;
-  ownerId: string;
-  active: boolean;
-  url: string;
-  createdAt: Date;
+  readonly identifier: string;
+  readonly ownerId: string;
+  readonly url: string;
+  readonly createdAt: Date;
+  private _active: boolean;
 
   constructor(ownerId: string, url: string) {
     this.identifier = new Identifier().id;
     this.ownerId = ownerId;
-    this.active = true;
+    this._active = true;
     this.url = url;
     this.createdAt = new Date();
+  }
+
+  public get active() {
+    return this._active;
+  }
+
+  public set active(value: boolean) {
+    this._active = value;
+  }
+
+  get getPhoto() {
+    return {
+      identifier: this.identifier,
+      ownerId: this.ownerId,
+      url: this.url,
+      active: this._active,
+      createdAt: this.createdAt
+    }
   }
 }
