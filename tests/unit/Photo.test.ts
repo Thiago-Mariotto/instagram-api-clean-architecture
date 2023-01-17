3b586778c6fc6971cbc1083bceaf31c3b8b16c1f
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-01-17 14:30:21 -0300

    test: verify if class has ben instace

--- tests/unit/Photo.test.ts
+++ tests/unit/Photo.test.ts
@@ -1,9 +1,12 @@
 import { expect } from "chai";
 import Photo from "../../src/domain/entity/Photo";
 
-describe('Photo', function () {
+describe('Photo entity', function () {
   it('Deve ser possível criar uma foto', function () {
-    const photo = new Photo('4da5e84f-2040-4f1e-8207-a8f834351685', '');
-    expect(photo).to.be.contains.all.keys(['identifier', 'ownerId', 'active', 'url', 'createdAt'])
+    const photo = new Photo('4da5e84f-2040-4f1e-8207-a8f834351685', 'www.google.com/images/123');
+    expect(photo).to.be.instanceof(Photo);
   });
+
+
+
 });
