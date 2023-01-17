15990faa99504f7b5ecbf6f51cab0908e3272030
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-01-17 20:12:55 -0300

    chore: implements get user by email or username

--- src/domain/repository/UserRepository.ts
+++ src/domain/repository/UserRepository.ts
@@ -5,6 +5,7 @@ interface UserRepository {
   getUserById(id: string): Promise<User | undefined>;
   updateUser(user: User): Promise<void>;
   removeUser(id: string): Promise<void>;
+  getUserByEmailOrUsername(email: string, username: string): Promise<User | undefined>;
 }
 
 export default UserRepository;
