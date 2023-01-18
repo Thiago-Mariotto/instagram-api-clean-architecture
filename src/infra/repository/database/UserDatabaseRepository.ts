66b3b94610965166c477737425412aba8369ece4
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-01-18 18:25:30 -0300

    refactor: change user attributes

--- src/infra/repository/database/UserDatabaseRepository.ts
+++ src/infra/repository/database/UserDatabaseRepository.ts
@@ -5,6 +5,11 @@ import Connection from '../../database/Connection';
 export default class UserDatabaseRepository implements UserRepository {
 
   constructor(readonly connection: Connection) { }
+  async getUserByUsername(username: string): Promise<User> {
+    const [result] = await this.connection.query('SELECT * FROM users WHERE username = ?',
+      [username]);
+    return result[0];
+  }
   updateUser(user: User): Promise<void> {
     throw new Error('Method not implemented.');
   }
@@ -13,9 +18,10 @@ export default class UserDatabaseRepository implements UserRepository {
   }
   async saveUser(user: User): Promise<void> {
     return await this.connection.query('INSERT INTO users (id, username, email, password) VALUES (?, ?, ?, ?)',
-      [user.id, user.username, user.email, user.password]);
+      [user.identifier, user.username, user.email, user.password]);
   }
-  async getUserById(id: string): Promise<User | undefined> {
-    return await this.connection.query('SELECT * FROM users WHERE id = ?', [id]);
+  async getUserById(id: string): Promise<User> {
+    const [result] = await this.connection.query('SELECT * FROM users WHERE id = ?', [id]);
+    return result[0];
   }
 }
