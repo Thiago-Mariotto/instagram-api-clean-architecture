908dcd436ddbe7dbc86ce90943a46cf5cc0aaaa3
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-01-18 18:25:51 -0300

    chore: change login method to use only username

--- src/infra/repository/memory/UserMemoryRepository.ts
+++ src/infra/repository/memory/UserMemoryRepository.ts
@@ -6,7 +6,7 @@ export default class UserMemoryRepository implements UserRepository {
   constructor() {
     this.users = [];
   }
-  getUserByEmailOrUsername(email: string, username: string): Promise<User | undefined> {
+  getUserByUsername(username: string): Promise<User> {
     throw new Error('Method not implemented.');
   }
   updateUser(user: User): Promise<void> {
@@ -15,15 +15,13 @@ export default class UserMemoryRepository implements UserRepository {
   removeUser(id: string): Promise<void> {
     throw new Error('Method not implemented.');
   }
-
   async getUsers(): Promise<User[]> {
     return this.users;
   }
-
-  async getUserById(id: string): Promise<User | undefined> {
-    return this.users.find(u => u.identifier === id);
+  async getUserById(id: string): Promise<User> {
+    const user = this.users.find(u => u.identifier === id);
+    return user as User;
   }
-
   async saveUser(user: User): Promise<void> {
     this.users.push(user);
   }
