f0461e2f221a269762dc6f09e00f8f0c9cc696d4
Thiago Mariotto <thiagomariotto@outlook.com.br>
2023-01-18 18:24:16 -0300

    refactor: remove arrow function error middleware

--- src/infra/http/ExpressAdapter.ts
+++ src/infra/http/ExpressAdapter.ts
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import Router from '../routes/MainRouter';
 export default class ExpressAdapter {
   app: any;
@@ -10,9 +10,6 @@ export default class ExpressAdapter {
     this.app = express();
     this.app.use(express.json());
     this.app.use(router.route);
-    this.app.use((error: any, req: Request, res: Response, next: NextFunction) => {
-      return res.status(error.status || 500).json({ message: error.message || 'Internal Server Error' });
-    });
   }
 
   listen(port: number): void {
